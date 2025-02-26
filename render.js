30d86ee99e0017585a71a255344e4f6d5ae56446
extremelystiff <41177088+extremelystiff@users.noreply.github.com>
2025-02-26 13:19:31 -0500

    Update render.js

--- render.js
+++ render.js
@@ -15,7 +15,7 @@ const CONFIG = {
     debug: 1,
     config: {
       iceServers: [
-        { urls: 'stun:stun.l.google.com:19302' }
+        { urls: 'stun:stun.l.google.com:19302' },
         { urls: 'stun:stun1.l.google.com:19302' },
         { urls: 'stun:stun2.l.google.com:19302' }
       ]
